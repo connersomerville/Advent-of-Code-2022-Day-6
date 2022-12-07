a10f1c9f00a4f85579f5a38e19080695f39e286e
Conner Somerville <5721950+connersomerville@users.noreply.github.com>
2022-12-07 22:28:46 +0000

    part 1

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { getLineReader } from "./reader.js";
+
+const args = process.argv.slice(2);
+const filePath = args[0] || "test/fixtures/input.txt";
+const lineReader = getLineReader({
+  filePath,
+});
+
+let buffer = "";
+
+lineReader.on("line", (line) => {
+  buffer += line;
+});
+
+lineReader.on("close", () => {
+  for (let start = 0, end = 4; end < buffer.length; start++, end++) {
+    const substring = buffer.slice(start, end);
+    const marker = new Set(substring.split("")).size === 4;
+
+    if (marker) {
+      console.log(`Marker found at ${end} substring ${substring}`);
+      break;
+    }
+  }
+});
